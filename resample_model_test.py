588bad7f0ff36e9e2f6483852dbf3a1317e500fc
xkchen <cxkast@gmail.com>
2019-09-22 15:42:58 +0800

    re-write "resample_modelu.py" for rule out "black line"

--- resample_model_test.py
+++ resample_model_test.py
@@ -1,104 +1,273 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
-import h5py
+import random
 import numpy as np
-import astropy.wcs as awc
-import astropy.units as U
-import astropy.constants as C
 import astropy.io.fits as fits
-from astropy import cosmology as apcy
-# resample part
+
+from resamp import gen
 from resample_modelu import down_samp
 from resample_modelu import sum_samp
+from scipy.interpolate import interp2d as interp2
 
-kpc2cm = U.kpc.to(U.cm)
-Mpc2pc = U.Mpc.to(U.pc)
-Mpc2cm = U.Mpc.to(U.cm)
-rad2asec = U.rad.to(U.arcsec)
-pc2cm = U.pc.to(U.cm)
-Lsun = C.L_sun.value*10**7
+pixel = 0.396
+def resamp_block():
+    """
+    mu : the pixel size ratio (mu = Pixel_new / Pixel_old)
+    """
+    data = fits.getdata('sub_img.fits', header = True)
+    img = data[0]
 
-# cosmology model
-Test_model = apcy.Planck15.clone(H0 = 67.74, Om0 = 0.311)
-H0 = Test_model.H0.value
-h = H0/100
-Omega_m = Test_model.Om0
-Omega_lambda = 1.-Omega_m
-Omega_k = 1.- (Omega_lambda + Omega_m)
+    eta = np.linspace(0.844, 1.185, 11)
+    for kk in range(len(eta)):
+        #mu = eta[kk]
+        mu = 0.84
 
-# cosmology model
-Test_model = apcy.Planck15.clone(H0 = 67.74, Om0 = 0.311)
-H0 = Test_model.H0.value
-h = H0/100
-Omega_m = Test_model.Om0
-Omega_lambda = 1.-Omega_m
-Omega_k = 1.- (Omega_lambda + Omega_m)
+        Nx = img.shape[1]
+        Ny = img.shape[0]
+        x_set = np.array( [random.randint(15, img.shape[1] - 15) for _ in range(2000)] )
+        y_set = np.array( [random.randint(15, img.shape[0] - 15) for _ in range(2000)] )
+        f_set = img[y_set, x_set] / pixel**2
 
-pixel = 0.396 # the pixel size in unit arcsec
-z_ref = 0.250 
-Jy = 10**(-23) # (erg/s)/cm^2
-f0 = 3631*10**(-23) # zero point in unit (erg/s)/cm^-2
+        ## single resample
+        '''
+        cx, cy, cimg = gen(img, 1, mu, Nx / 2, Ny / 2 ) # previous code
+        if mu > 1:
+            cimg = cimg[1:, 1:]
+        elif mu == 1:
+            cimg = cimg[1:-1, 1:-1]
+        else:
+            cimg = cimg
+        cx = np.int(cx)
+        cy = np.int(cy)
+        '''
+        if mu > 1:
+            cimg, cx, cy = sum_samp(mu, mu, img, Nx / 2, Ny / 2)
+        else:
+            cimg, cx, cy = down_samp(mu, mu, img, Nx / 2, Ny / 2)
+        cx = np.int(cx)
+        cy = np.int(cy)
 
-with h5py.File('/home/xkchen/mywork/ICL/code/sample_catalog.h5') as f:
-    catalogue = np.array(f['a'])
-z = catalogue[0]
-ra = catalogue[1]
-dec = catalogue[2]
+        x_set1 = np.array( [np.int( ll / mu ) for ll in x_set] )
+        y_set1 = np.array( [np.int( ll / mu ) for ll in y_set] )
+        f_set1 = cimg[y_set1, x_set1] / (mu*pixel)**2
 
-# resample process
-x0 = np.linspace(0,2047,2048)
-y0 = np.linspace(0,1488,1489)
-pix_id = np.array(np.meshgrid(x0,y0))
+        fig = plt.figure(figsize = (16, 8))
+        fig.suptitle('Pixel scale %.3f' % mu)
+        ax0 = plt.subplot(121)
+        ax1 = plt.subplot(122)
+        ax0.set_title('original image')
 
-def resamp_test():
-    import time
-    t0 = time.time()
+        tf = ax0.imshow(img / pixel**2, cmap = 'Greys', vmin = 1e-5, vmax = 1e2, origin = 'lower', norm = mpl.colors.LogNorm())
+        plt.colorbar(tf, ax = ax0, fraction = 0.047, pad = 0.01, label = '$Pixel \; SB \, [nmaggy/arcsec^2]$')
+        ax1.set_title('original image')
+        tf = ax1.imshow(cimg, cmap = 'Greys', vmin = 1e-5, vmax = 1e2, origin = 'lower', norm = mpl.colors.LogNorm())
+        plt.colorbar(tf, ax = ax1, fraction = 0.047, pad = 0.01, label = '$Pixel \; SB \, [nmaggy/arcsec^2]$')
 
-    kd = 0
-    zg = z[kd]
-    rag = ra[kd]
-    decg = dec[kd]
+        plt.tight_layout()
+        plt.savefig('re_correct_resample_%.3f_scale.png' % mu, dpi = 300)
+        plt.close()
+        #raise
+        ## inverse resample
+        kap = 1 / mu
+        '''
+        c2x, c2y, c2img = gen(cimg, 1, kap, np.int(cx), np.int(cy) ) # previous code
+        if mu < 1:
+            c2img = c2img[1:,1:]
+        elif mu == 1:
+            c2img = c2img[1:-1, 1:-1]
+        else:
+            c2img = c2img
+        '''
+        if kap > 1:
+            c2img, c2x, c2y = sum_samp(kap, kap, cimg, cx, cy)
+        else:
+            c2img, c2x, c2y = down_samp(kap, kap, cimg, cx, cy)
+        c2x = np.int(c2x)
+        c2y = np.int(c2y)
+        x_set2 = np.array( [np.int( ll / kap ) for ll in x_set1] )
+        y_set2 = np.array( [np.int( ll / kap ) for ll in y_set1] )
+        f_set2 = c2img[y_set2, x_set2] / pixel**2
+        '''
+        plt.figure()
+        ax = plt.subplot(111)
+        ax.set_title('Pixel scale %.2f' % mu)
+        ax.scatter(f_set, f_set1, s = 10, marker = 'o', facecolors = 'b', edgecolors = 'b', label = 'single resample', alpha = 0.5)
+        ax.scatter(f_set, f_set2, s = 10, marker = 'o', facecolors = 'g', edgecolors = 'g', label = 'twice resample', alpha = 0.5)
+        ax.plot(f_set, f_set, 'r--', label = 'slope = 1', alpha = 0.5)
+        ax.set_xlabel('Pixel SB [Before resampling]')
+        ax.set_ylabel('Pixel SB [After resampling]')
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.legend(loc = 2)
+        plt.savefig('pix_SB_scale_%.2f.png' % mu, dpi = 300)
+        plt.show()
+        '''
+        cl0 = (c2img.shape[0] - 3) / 2
+        cl1 = (c2img.shape[1] - 3) / 2
+        cut_0 = img[np.int(Ny/2 - cl0 - 1): np.int(Ny/2 + cl0), np.int(Nx/2 - cl1 - 1): np.int(Nx/2 + cl1)]
+        cut_2 = c2img[np.int(c2y - cl0): np.int(c2y + cl0 + 1), np.int(c2x - cl1): np.int(c2x + cl1 + 1)]
+        ddbs = (cut_2 - cut_0) / cut_0
+
+        fig = plt.figure(figsize = (16, 8))
+        fig.suptitle('pixel scale %.3f' % mu)
+        ax0 = plt.subplot(221)
+        ax1 = plt.subplot(222)
+        ax2 = plt.subplot(223)
+        ax3 = plt.subplot(224)
+
+        ax0.set_title('original image')
+        tf = ax0.imshow(img / pixel**2, cmap = 'Greys', origin = 'lower', vmin = 1e-5, vmax = 1e2, norm = mpl.colors.LogNorm())
+        plt.colorbar(tf, ax = ax0, fraction = 0.035, pad = 0.01, label = '$Pixel \; SB \, [nmaggy/arcsec^2]$')
+
+        ax1.set_title('single resample image')
+        tf = ax1.imshow(cimg / (mu * pixel)**2, cmap = 'Greys', origin = 'lower', vmin = 1e-5, vmax = 1e2, norm = mpl.colors.LogNorm())
+        plt.colorbar(tf, ax = ax1, fraction = 0.035, pad = 0.01, label = '$Pixel \; SB \, [nmaggy/arcsec^2]$')
+
+        ax2.set_title('twice resample image')
+        tf = ax2.imshow(c2img / pixel**2, cmap = 'Greys', origin = 'lower', vmin = 1e-5, vmax = 1e2, norm = mpl.colors.LogNorm())
+        plt.colorbar(tf, ax = ax2, fraction = 0.035, pad = 0.01, label = '$Pixel \; SB \, [nmaggy/arcsec^2]$')
+
+        ax3.set_title('twice resample - original / origin')
+        from matplotlib import cm
+        tf = ax3.imshow(ddbs, origin = 'lower', norm = mpl.colors.SymLogNorm(linthresh=0.01), cmap=cm.PRGn)
+        plt.colorbar(tf, ax = ax3, fraction = 0.035, pad = 0.01)
+
+        plt.tight_layout()
+        plt.subplots_adjust(bottom = 0.1, right = 0.8, top = 0.9)
+        plt.savefig('Twice-resample_scale_%.3f.png' % mu, dpi = 300)
+        plt.show()
+
+        raise
+    return
+
+def project():
+    import h5py
+    import astropy.wcs as awc
+    from matplotlib import cm
+    from reproject import reproject_exact
+
+    with h5py.File('/home/xkchen/mywork/ICL/code/sample_catalog.h5') as f:
+        catalogue = np.array(f['a'])
+    z = catalogue[0]
+    ra = catalogue[1]
+    dec = catalogue[2]
+
+    # get a example of "wcs" information
     load = '/home/xkchen/mywork/ICL/data/total_data/'
-    file = 'frame-r-ra%.3f-dec%.3f-redshift%.3f.fits.bz2' % (rag, decg, zg)
-    data = fits.open(load + file)
+    file = load + 'frame-r-ra%.3f-dec%.3f-redshift%.3f.fits.bz2' % (ra[0], dec[0], z[0])
+    data = fits.open(file)
     img = data[0].data
-    Head = data[0].header
-    wcs = awc.WCS(Head)
-    cx, cy = wcs.all_world2pix(rag*U.deg, decg*U.deg, 1)
+    wcs = awc.WCS(data[0].header)
+    '''
+    # cut region for detail test
+    nl = 50
+    sub_img = img[758 - nl: 758 + nl, 1943 - nl: 1943 + nl]
+    keys = ['SIMPLE', 'BITPIX', 'NAXIS', 'NAXIS1', 'NAXIS2', 'CRPIX1', 'CRPIX2', 'CENTER_X', 'CENTER_Y', 'UNIT', 'P_SCALE']
+    values = ['T', 32, 2, sub_img.shape[1], sub_img.shape[0], np.int(nl), np.int(nl), np.int(nl), np.int(nl), 'NMAGGY', 0.396]
+    ff = dict(zip(keys, values))
+    fil = fits.Header(ff)
+    fits.writeto('sub_img_1.fits', sub_img, header = fil, overwrite=True)
+    '''
+    eta = 0.95
+    #eta = 0.844# New_pix / Old_pix
 
-    Da = Test_model.angular_diameter_distance(zg).value
-    Da_ref = Test_model.angular_diameter_distance(z_ref).value
+    # cut a region for test
+    nl = 50
+    sub_img = img[939 - nl: 939 + nl, 1407 - nl: 1407 + nl]
+    #sub_img = np.ones((nl, nl), dtype = np.float) # constant field
 
-    eta = 0.8
-    mu = 1 / eta
-    if eta > 1:
-        resam_data, nx, ny = sum_samp(eta, eta, img, cx, cy)
-    else:
-        resam_data, nx, ny = down_samp(eta, eta, img, cx, cy)
-    t1 = time.time() - t0
-    print(t1)
+    # initial "wcs" for the cut region
+    Nx_0 = np.int(sub_img.shape[1])
+    Ny_0 = np.int(sub_img.shape[0])
+    Cx_0 = np.int(nl)
+    Cy_0 = np.int(nl)
+    typ1_0 = data[0].header['CTYPE1']
+    typ2_0 = data[0].header['CTYPE2']
+    cra_0 = data[0].header['CRVAL1']
+    cdec_0 = data[0].header['CRVAL2']
+    CD11_0 = data[0].header['CD1_1']
+    CD12_0 = data[0].header['CD1_2']
+    CD21_0 = data[0].header['CD2_1']
+    CD22_0 = data[0].header['CD2_2']
 
-    plt.figure(figsize = (16, 8))
-    gs = gridspec.GridSpec(1, 2, width_ratios = [1, 1])
-    ax = plt.subplot(gs[0])
-    bx = plt.subplot(gs[1])
-    ax.imshow(img, cmap = 'Greys', vmin = 1e-3, origin = 'lower', norm = mpl.colors.LogNorm())
-    ax.scatter(cx, cy, s = 10, marker = 'o', facecolors = '', edgecolors = 'r', linewidth = 0.5, alpha = 0.5)
-    bx.imshow(resam_data, cmap = 'Greys', vmin = 1e-3, origin = 'lower', norm = mpl.colors.LogNorm())
-    bx.scatter(nx, ny, s = 10, marker = 'o', facecolors = '', edgecolors = 'r', linewidth = 0.5, alpha = 0.5)
+    keys = ['SIMPLE', 'BITPIX', 'NAXIS', 'NAXIS1', 'NAXIS2', 'CRPIX1', 'CRPIX2', 'CTYPE1', 'CTYPE2', 
+            'CRVAL1', 'CRVAL2', 'CD1_1', 'CD1_2', 'CD2_1', 'CD2_2', ]
+    values = ['T', 32, 2, Nx_0, Ny_0, Cx_0, Cy_0, typ1_0, typ2_0, cra_0, cdec_0, CD11_0, CD12_0, CD21_0, CD22_0]
+    ff = dict(zip(keys, values))
+    fil = fits.Header(ff)
+    wcs0 = awc.WCS(fil)
+    img_set = fits.ImageHDU(sub_img / pixel**2, header = fil)
+
+    # set the wcs information for resample img
+    Nx = np.int(Nx_0 / eta)
+    Ny = np.int(Ny_0 / eta)
+    Cx = np.int(nl / eta)
+    Cy = np.int(nl / eta)
+    typ1 = data[0].header['CTYPE1']
+    typ2 = data[0].header['CTYPE2']
+    cra = data[0].header['CRVAL1']
+    cdec = data[0].header['CRVAL2']
+    CD11 = data[0].header['CD1_1'] * eta
+    CD12 = data[0].header['CD1_2'] * eta
+    CD21 = data[0].header['CD2_1'] * eta
+    CD22 = data[0].header['CD2_2'] * eta
+
+    keys = ['SIMPLE', 'BITPIX', 'NAXIS', 'NAXIS1', 'NAXIS2', 'CRPIX1', 'CRPIX2', 'CTYPE1', 'CTYPE2', 
+            'CRVAL1', 'CRVAL2', 'CD1_1', 'CD1_2', 'CD2_1', 'CD2_2', ]
+    values = ['T', 32, 2, Nx, Ny, Cx, Cy, typ1, typ2, cra, cdec, CD11, CD12, CD21, CD22]
+    ff = dict(zip(keys, values))
+    fil_1 = fits.Header(ff)
+    wcs1 = awc.WCS(fil_1)
+
+    # resample 1-- single resample; 2-- twice resample (pixel scale is the initial value)
+    cimg1, pix_foot1 = reproject_exact(img_set, output_projection = wcs1, shape_out = (Ny, Nx))
+
+    img_set1 = fits.ImageHDU(cimg1, header = fil)
+    cimg2, pix_foot2 = reproject_exact(img_set1, output_projection = wcs0, shape_out = (Ny_0, Nx_0))
+
+    cl0 = (cimg2.shape[0] - 15) / 2
+    cl1 = (cimg2.shape[1] - 15) / 2
+    cut_0 = sub_img[np.int(Cy_0 - cl0): np.int(Cy_0 + cl0 + 1), np.int(Cx_0 - cl1): np.int(Cx_0 + cl1 + 1)]
+    cut_2 = cimg2[np.int(Cy_0 - cl0 + 3): np.int(Cy_0 + cl0 + 4), np.int(Cx_0 - cl1 + 3): np.int(Cx_0 + cl1 + 4)] # eta = 0.95
+    #cut_2 = cimg2[np.int(Cy_0 - cl0 + 7): np.int(Cy_0 + cl0 + 8), np.int(Cx_0 - cl1 + 7): np.int(Cx_0 + cl1 + 8)] # eta = 0.844
+    ddbs = (cut_2 - cut_0) / cut_0
+
+    fig = plt.figure(figsize = (16, 8))
+    fig.suptitle('pixel scale %.2f' % eta)
+    ax0 = plt.subplot(221)
+    ax1 = plt.subplot(222)
+    ax2 = plt.subplot(223)
+    ax3 = plt.subplot(224)
+
+    ax0.set_title('original image')
+    tf = ax0.imshow(sub_img, cmap = 'Greys', origin = 'lower', vmin = 1e-5, vmax = 1e2, norm = mpl.colors.LogNorm())
+    plt.colorbar(tf, ax = ax0, fraction = 0.035, pad = 0.01, label = '$Pixel \; SB \, [nmaggy/arcsec^2]$')
+
+    ax1.set_title('single resample image')
+    tf = ax1.imshow(cimg1, cmap = 'Greys', origin = 'lower', vmin = 1e-5, vmax = 1e2, norm = mpl.colors.LogNorm())
+    plt.colorbar(tf, ax = ax1, fraction = 0.035, pad = 0.01, label = '$Pixel \; SB \, [nmaggy/arcsec^2]$')
+
+    ax2.set_title('twice resample image')
+    tf = ax2.imshow(cimg2, cmap = 'Greys', origin = 'lower', vmin = 1e-5, vmax = 1e2, norm = mpl.colors.LogNorm())
+    plt.colorbar(tf, ax = ax2, fraction = 0.035, pad = 0.01, label = '$Pixel \; SB \, [nmaggy/arcsec^2]$')
+
+    ax3.set_title('twice resample - original / origin')
+    tf = ax3.imshow(ddbs, origin = 'lower', norm = mpl.colors.SymLogNorm(linthresh=0.01), cmap=cm.PRGn)
+    plt.colorbar(tf, ax = ax3, fraction = 0.035, pad = 0.01)
 
     plt.tight_layout()
-    plt.savefig('/home/xkchen/mywork/ICL/code/resamp_test.png', dpi = 600)
+    plt.subplots_adjust(bottom = 0.1, right = 0.8, top = 0.9)
+    plt.savefig('Reproject_scale_%.2f.png' % eta, dpi = 300)
     plt.show()
-    plt.close()
 
     raise
+
     return
 
 def main():
-    resamp_test()
-    
+    resamp_block()
+    #project()
+
 if __name__ == "__main__":
     main()
--- resample_modelu.py
+++ resample_modelu.py
@@ -1,17 +1,79 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-a = np.ones((5,5), dtype = np.float)
-a[1,2] = 0
-a[3,3] = 0
- 
-cx0 = 2
-cy0 = 2
-m1 = 1.2
-m2 = 1.2 # for up-resamp
-m3 = 0.8
-m4 = 0.8 # for down-resamp
+def down_samp(m3, m4, data, cx0, cy0):
+    mx = m3
+    my = m4
+
+    Mrr = np.zeros((data.shape[0] + 1, data.shape[1] + 1), dtype = np.float)
+    Mrr[:-1, :-1] = data * 1
+
+    sNy = data.shape[0]
+    sNx = data.shape[1]
+
+    Nx = np.int(sNx * (1/mx))
+    Ny = np.int(sNy * (1/my))
+    cx = cx0 / mx
+    cy = cy0 / my
+    cpos = np.array([cx, cy])
+
+    resam = np.zeros((Ny, Nx), dtype = np.float)
+
+    eps = 1e-12
+    for k in range(Ny):
+
+        for l in range(Nx):
+
+            idy = (2*k + 1) * my / 2
+            idx = (2*l + 1) * mx / 2
+
+            sor_y = np.int(k // (1/my))
+            sor_x = np.int(l // (1/mx))
+
+            xa = (idx - mx/2) - sor_x
+            ## set "0" if the miuns resulte is small then eps
+            # avoid failure of condition identifying caused by calculation accuracy
+            it = eps < np.abs(xa)
+            xa = xa * it
+
+            xb = (idx + mx/2) - (sor_x + 1)
+            it = eps < np.abs(xb)
+            xb = xb * it
+
+            ya = (idy - my/2) - sor_y
+            it = eps < np.abs(ya)
+            ya = ya * it
+
+            yb = (idy + my/2) - (sor_y + 1)
+            it = eps < np.abs(yb)
+            yb = yb * it
+            '''
+            if (k % 20 == 0) & (l % 20 == 0):
+                print('*' * 20)
+                print('k, l = ', k, l)
+                print('y, x = ', sor_y, sor_x)
+                print('xa, xb = ', xa, xb)
+                print('ya, yb = ', ya, yb)
+            '''
+            if (xa >= 0) & (xb <= 0) & (ya >= 0) & (yb <= 0):
+                resam[k,l] = mx * my * Mrr[sor_y, sor_x]
+
+            elif (xa >= 0) & (xb <= 0) & (ya >= 0) & (yb > 0):
+                resam[k,l] = yb * mx * Mrr[sor_y + 1, sor_x] + (my - yb) * mx * Mrr[sor_y, sor_x]
+
+            elif (xa >= 0) & (xb > 0) & (ya >= 0) & (yb <= 0):
+                resam[k,l] = xb * my * Mrr[sor_y, sor_x + 1] + (mx - xb) * my * Mrr[sor_y, sor_x]
+
+            elif (xa >= 0) & (xb > 0) & (ya >= 0) & (yb > 0):
+                resam[k,l] = (mx - xb) * (my - yb) * Mrr[sor_y, sor_x] + xb * (my - yb) * Mrr[sor_y, sor_x + 1] \
+                + yb * (mx - xb) * Mrr[sor_y + 1, sor_x] + xb * yb * Mrr[sor_y + 1, sor_x + 1]
+
+            else:
+                continue
+
+    return resam, cx, cy
 
 def sum_samp(m1, m2, data, cx0, cy0):
     mx = m1
@@ -19,9 +81,7 @@ def sum_samp(m1, m2, data, cx0, cy0):
     a = data
     N0x = a.shape[1]
     N0y = a.shape[0]
-    x0 = np.linspace(0, N0x-1, N0x)
-    y0 = np.linspace(0, N0y-1, N0y)
-    M = np.meshgrid(x0,y0)
+
     N2 = np.int(N0y * (1 / my))
     N1 = np.int(N0x * (1 / mx))
 
@@ -30,25 +90,34 @@ def sum_samp(m1, m2, data, cx0, cy0):
     cpos = np.array([cx, cy])
     resamp = np.zeros((N2 + 1, N1 + 1), dtype = np.float)
 
-    sig = N0y* N0x // 10
+    eps = 1e-12
     for k in range(N0y):
         for l in range(N0x):
-
-            if (k * N0x + l) % sig == 0:
-                print( "%3.0f%%"%( (k *N0x + l) / (N0y * N0x) * 100 ) )
 
             in_mx = 1 / mx
             in_my = 1 / my
 
             idy = (2*k + 1) * in_my /2
             idx = (2*l + 1) * in_mx /2
+       
             sor_y = np.int(k // my)
             sor_x = np.int(l // mx)
 
-            xa = (idx - in_mx /2) - sor_x
-            xb = (idx + in_mx /2) - (sor_x + 1)
-            ya = (idy - in_my /2) - sor_y
-            yb = (idy + in_my /2) - (sor_y + 1)
+            xa = (idx - in_mx/2) - sor_x
+            it = eps < np.abs(xa)
+            xa = xa * it
+
+            xb = (idx + in_mx/2) - (sor_x + 1)
+            it = eps < np.abs(xb)
+            xb = xb * it
+
+            ya = (idy - in_my/2) - sor_y
+            it = eps < np.abs(ya)
+            ya = ya * it
+
+            yb = (idy + in_my/2) - (sor_y + 1)
+            it = eps < np.abs(yb)
+            yb = yb * it
 
             S_pix = in_my * in_mx
             if (xa >= 0) & (xb <= 0) & (ya >= 0) & (yb <= 0):
@@ -73,76 +142,96 @@ def sum_samp(m1, m2, data, cx0, cy0):
 
     return resamp, cx, cy
 
-def down_samp(m3, m4, data, cx0, cy0):
-    mx = m3
-    my = m4
-    a = data
-    sNy = a.shape[0]
-    sNx = a.shape[1]
-    
-    x0 = np.linspace(0,sNx-1,sNx)
-    y0 = np.linspace(0,sNy-1,sNy)
-    M0 = np.meshgrid(x0,y0)
-    
-    N1x = np.int(sNx*(1/mx))
-    N1y = np.int(sNy*(1/my))
-    cx = cx0/mx
-    cy = cy0/my
-    cpos = np.array([cx, cy])
-    Ny = N1y
-    Nx = N1x
-    resamp = np.zeros((Ny, Nx), dtype = np.float)
-
-    sig = Ny * Nx // 10
-    for k in range(Ny):
-
-        for l in range(Nx):
-
-            if (k * Nx + l) % sig == 0:
-                print( "%3.0f%%"%( (k *Nx + l) / (Ny * Nx) * 100 ) )
-
-            idy = (2*k + 1) * my/2
-            idx = (2*l + 1) * mx/2
-            sor_y = np.int(k // (1/my))
-            sor_x = np.int(l // (1/mx))
-
-            xa = (idx - mx/2) - sor_x
-            xb = (idx + mx/2) - (sor_x + 1)
-            ya = (idy - my/2) - sor_y
-            yb = (idy + my/2) - (sor_y + 1)
-
-            if (xa >= 0) & (xb <= 0) & (ya >= 0) & (yb <= 0):
-                resamp[k,l] = mx * my * a[sor_y, sor_x]
-
-            elif (xa >= 0) & (xb <= 0) & (ya >= 0) & (yb > 0):
-                resamp[k,l] = yb * mx * a[sor_y + 1, sor_x] + (my - yb) * mx * a[sor_y, sor_x]
-
-            elif (xa >= 0) & (xb > 0) & (ya >= 0) & (yb <= 0):
-                resamp[k,l] = xb * my * a[sor_y, sor_x + 1] + (mx - xb) * my * a[sor_y, sor_x]
-
-            elif (xa >= 0) & (xb > 0) & (ya >= 0) & (yb > 0):
-                resamp[k,l] = (mx - xb) * (my - yb) * a[sor_y, sor_x] + xb * (my - yb) * a[sor_y, sor_x + 1] \
-                + yb * (mx - xb) * a[sor_y + 1, sor_x] + xb * yb * a[sor_y + 1, sor_x + 1]
-
-            else:
-                continue
-
-    return resamp, cx, cy
-
 def main():
-    resam1 = sum_samp(m1, m2, a, cx0, cy0)[0]
-    resam2 = down_samp(m3, m4, a, cx0, cy0)[0]
-    print('before = ', a)
-    print('*'*10)
+    cx0 = 2
+    cy0 = 2
 
-    plt.figure()
-    ax = plt.subplot(221)
-    bx = plt.subplot(222)
-    cx = plt.subplot(223)
-    ax.imshow(a, origin = 'lower')
-    bx.imshow(resam1, origin = 'lower')
-    cx.imshow(resam2, origin = 'lower')
+    A = np.random.random((100, 100))
+    #A = np.ones((100, 100), dtype = np.float)
+
+    import astropy.io.fits as fits
+    data = fits.open('sub_img.fits')
+    #A = data[0].data
+
+    m1 = 1.05
+    m2 = 0.95
+    t0 = time.time()
+    #resam = sum_samp(m1, m1, A, cx0, cy0)[0]
+    resam = down_samp(m2, m2, A, cx0, cy0)[0]
+    t1 = time.time() - t0
+    print('t = ', t1)
+
+    print('max = ', np.max(resam))
+    print('min = ', np.min(resam))
+    print('mean = ', np.mean(resam))
+    fig = plt.figure()
+    #fig.suptitle('scale %.2f' % m1)
+    #fig.suptitle('scale %.2f' % m2)
+    ax = plt.subplot(121)
+    bx = plt.subplot(122)
+    ax.set_title('origin')
+    ax.imshow(A, cmap = 'Greys', vmin = 1e-4, vmax = 1e2, origin = 'lower', norm = mpl.colors.LogNorm())
+    bx.set_title('resample')
+    bx.imshow(resam, cmap = 'Greys', vmin = 1e-4, vmax = 1e2, origin = 'lower', norm = mpl.colors.LogNorm())
+    plt.tight_layout()
+    plt.savefig('test.png', dpi = 300)
+    #plt.savefig('test_%.2f.png' % m1, dpi = 300)
+    #plt.savefig('test_%.2f.png' % m2, dpi = 300)
     plt.show()
+
+    '''
+    t0 = time.time()
+    resam = down_samp(m2, m2, A, cx0, cy0)[0]
+    t1 = time.time() - t0
+
+    x_arr = np.linspace(0,9,10)
+    y_arr = np.ones(10, dtype = np.float) * 20
+    flux_res0 = resam[20, :10]
+    flux_res1 = resam[19, :10]
+    flux_res2 = resam[21, :10]
+
+    flux_set0 = np.array([-0.010080413818359375, 0.021439361572265624, -0.019343261718749998, 0.01169525146484375, -0.030339813232421874,
+                        0.005679473876953124, 0.01018768310546875, -0.0018192291259765624, 0.0029412078857421887, 0.021461105346679686]) # center of line
+    flux_set1 = np.array([0.008923645019531249, -0.045140075683593746, 0.011443023681640624, 0.010439910888671876, -0.013578262329101563,
+                        -0.009075126647949218, -0.008577919006347657, 0.016951446533203123, 0.018959121704101564, -0.02608528137207031])
+    flux_set2 = np.array([0.030467376708984372, -0.006287384033203126, -0.022661132812500002, -0.03761138916015625, -0.021346130371093747,
+                        -0.023704833984374996, 0.03153961181640626, 0.003881301879882814, -0.022187271118164063, -0.02216011047363281])
+
+    fig = plt.figure()
+    ax = plt.subplot(121)
+    bx = plt.subplot(122)
+    ax.set_title('origin')
+    ax.imshow(A, cmap = 'Greys', vmin = 1e-5, vmax = 1e2, origin = 'lower', norm = mpl.colors.LogNorm())
+    bx.set_title('resample')
+    bx.imshow(resam, cmap = 'Greys', vmin = 1e-5, vmax = 1e2, origin = 'lower', norm = mpl.colors.LogNorm())
+    plt.tight_layout()
+    plt.savefig('test.png', dpi = 300)
+    plt.show()
+
+    plt.figure(figsize = (16,8))
+    ax = plt.subplot(122)
+    bx = plt.subplot(121)
+    ax.set_title('Lattice pixel Flux check')
+    ax.plot(flux_res0, flux_set0, 'ro', label = 'pixel on the lattice', alpha = 0.5)
+    ax.plot(flux_res1, flux_set1, 'gs', label = 'pixel below the lattice', alpha = 0.5)
+    ax.plot(flux_res2, flux_set2, 'b^', label = 'pixel above the lattice', alpha = 0.5)
+    xtik = ax.get_xticks()
+    ax.plot(xtik[1:-1], xtik[1:-1], 'k--', label = 'slope = 1', alpha = 0.5)
+    ax.set_xlabel('Flux on resample image')
+    ax.set_ylabel('Flux from theory')
+    ax.legend(loc = 4)
+
+    bx.set_title('resample with scale %.3f' % m2)
+    bx.imshow(resam, cmap = 'Greys', vmin = 1e-5, vmax = 1e2, origin = 'lower', norm = mpl.colors.LogNorm())
+    bx.scatter(x_arr, y_arr, s = 5, marker = 'o', facecolors = 'r', edgecolors = 'r', label = 'pixel on the lattice', alpha = 0.5)
+    bx.scatter(x_arr, y_arr - 1, s = 5, marker = 's', facecolors = 'g', edgecolors = 'g', label = 'pixel below the lattice', alpha = 0.5)
+    bx.scatter(x_arr, y_arr + 1, s = 5, marker = '^', facecolors = 'b', edgecolors = 'b', label = 'pixel above the lattice', alpha = 0.5)
+    bx.legend(loc = 1)
+
+    plt.tight_layout()
+    plt.savefig('points_check.png', dpi = 300)
+    plt.close()
+    '''
     raise
 
 if __name__ == "__main__":
