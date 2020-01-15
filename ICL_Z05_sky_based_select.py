159340b83f28a13573078760f880dbc104b647cd
xkchen <cxkast@gmail.com>
2020-01-15 20:40:04 +0800

    --

---
+++ ICL_Z05_sky_based_select.py
@@ -0,0 +1,120 @@
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+import matplotlib.gridspec as gridspec
+
+import astropy.units as U
+import astropy.constants as C
+from astropy import cosmology as apcy
+
+from dustmaps.sfd import SFDQuery
+from extinction_redden import A_wave
+from astropy.coordinates import SkyCoord
+from resample_modelu import sum_samp, down_samp
+from light_measure import light_measure, flux_recal
+
+import h5py
+import numpy as np
+import pandas as pds
+import astropy.wcs as awc
+import subprocess as subpro
+import astropy.io.ascii as asc
+import astropy.io.fits as fits
+
+from mpi4py import MPI
+commd = MPI.COMM_WORLD
+rank = commd.Get_rank()
+cpus = commd.Get_size()
+import time
+
+kpc2cm = U.kpc.to(U.cm)
+Mpc2pc = U.Mpc.to(U.pc)
+Mpc2cm = U.Mpc.to(U.cm)
+rad2asec = U.rad.to(U.arcsec)
+pc2cm = U.pc.to(U.cm)
+Lsun = C.L_sun.value*10**7
+# cosmology model
+Test_model = apcy.Planck15.clone(H0 = 67.74, Om0 = 0.311)
+H0 = Test_model.H0.value
+h = H0 / 100
+Omega_m = Test_model.Om0
+Omega_lambda = 1.-Omega_m
+Omega_k = 1.- (Omega_lambda + Omega_m)
+
+pixel = 0.396
+z_ref = 0.250
+Da_ref = Test_model.angular_diameter_distance(z_ref).value
+Jy = 10**(-23) # (erg/s)/cm^2
+f0 = 3631 * Jy # zero point in unit (erg/s)/cm^-2
+R0 = 1 # Mpc
+Angu_ref = (R0 / Da_ref) * rad2asec
+Rpp = Angu_ref / pixel
+
+load = '/mnt/ddnfs/data_users/cxkttwl/ICL/data/'
+tmp = '/mnt/ddnfs/data_users/cxkttwl/PC/'
+band = ['r', 'g', 'i', 'u', 'z']
+l_wave = np.array([6166, 4686, 7480, 3551, 8932])
+mag_add = np.array([0, 0, 0, -0.04, 0.02])
+
+Rv = 3.1
+sfd = SFDQuery()
+
+def imgs_cut(band_id, sub_z, sub_ra, sub_dec):
+
+	ii = np.int(band_id)
+	zn = len(sub_z)
+	for k in range(zn):
+		ra_g = sub_ra[k]
+		dec_g = sub_dec[k]
+		z_g = sub_z[k]
+
+		data = fits.getdata(load + 
+			'resample/Zibetti/A_mask/frame-%s-ra%.3f-dec%.3f-redshift%.3f.fits' % (band[ii], ra_g, dec_g, z_g), header = True)
+		img = data[0]
+		BCGx, BCGy = data[1]['CENTER_X'], data[1]['CENTER_Y']
+		RA0, DEC0 = data[1]['CRVAL1'], data[1]['CRVAL2']
+
+		xc, yc = np.int(img.shape[1] / 2), np.int(img.shape[0] / 2)
+		re_img = img[yc - np.int(Rpp): yc + np.int(Rpp), xc - np.int(1.3 * Rpp): xc + np.int(1.3 * Rpp)]
+
+		New_bcgx = BCGx - (xc - np.int(1.3 * Rpp))
+		New_bcgy = BCGy - (yc - np.int(Rpp))
+
+		Lx = re_img.shape[1]
+		Ly = re_img.shape[0]
+		Crx = np.int(1.3 * Rpp)
+		Cry = np.int(Rpp)
+
+		keys = ['SIMPLE','BITPIX','NAXIS','NAXIS1','NAXIS2','CRPIX1','CRPIX2','CENTER_X','CENTER_Y',
+				'CRVAL1','CRVAL2','CENTER_RA','CENTER_DEC','ORIGN_Z', 'P_SCALE']
+		value = ['T', 32, 2, Lx, Ly, Crx, Cry, New_bcgx, New_bcgy, RA0, DEC0, ra_g, dec_g, z_g, pixel]
+		ff = dict(zip(keys,value))
+		fil = fits.Header(ff)
+		fits.writeto(load + 
+			'Z05_record/select_set/imgs/cut_edge-%s-ra%.3f-dec%.3f-redshift%.3f.fits' % (band[ii], ra_g, dec_g, z_g), re_img, header = fil, overwrite = True)
+
+	return
+
+def main():
+	N_sum = np.array([2013, 2008, 2002, 2008, 2009]) ## center 1Mpc
+	### cut imgs edge
+	for kk in range(3):
+
+		with h5py.File(load + 'sky_select_img/%s_band_%d_imgs_sky_select.h5' % (band[kk], N_sum[kk]), 'r') as f:
+			set_array = np.array(f['a'])
+		set_ra, set_dec, set_z = set_array[0,:], set_array[1,:], set_array[2,:]
+
+		zN = len(set_z)
+		m, n = divmod(zN, cpus)
+		N_sub0, N_sub1 = m * rank, (rank + 1) * m
+		if rank == cpus - 1:
+			N_sub1 += n
+
+		imgs_cut(kk, set_z[N_sub0 :N_sub1], set_ra[N_sub0 :N_sub1], set_dec[N_sub0 :N_sub1])
+		commd.Barrier()
+
+	raise
+
+if __name__ == "__main__":
+	main()
